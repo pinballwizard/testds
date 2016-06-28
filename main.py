b186f7f59a181d67b718fc209e87df49a8778705
pinballwizard <menstenebris@gmail.com>
2016-06-28 16:50:57 +0700

    add day choice

--- main.py
+++ main.py
@@ -18,18 +18,13 @@ def fix_24(item):
     return item
 
 
-def all_for_day(day, dc):
-    return []
+def all_for_day(date, dc):
+    return scipy.array([item for item in dc if item[0].date() == date])
 
-if __name__ == '__main__':
-    es = open_csv('ES.csv')[1:]
-    # es = scipy.array(es)
-    es = list(map(fix_24, es))
-    dc = scipy.array(es)
-    day_tuple = set([item[0].date() for item in dc])
-    print(day_tuple)
+
+def my_plot(*data):
     fig, ax = plt.subplots()
-    ax.plot_date(dc[:, 0], dc[:,2])
+    ax.plot_date(*data)
     # ax.set_xlim(dc[0, 0], dc[-1, 0])
     # ax.xaxis.set_major_locator(DayLocator())
     # ax.xaxis.set_minor_locator(HourLocator(scipy.arange(0, 25, 6)))
@@ -40,5 +35,14 @@ if __name__ == '__main__':
     fig.autofmt_xdate()
     plt.show()
 
-    # plt.plot(dc[:,0], dc[:,1])
-    # print(dc[:,1])
+
+if __name__ == '__main__':
+    es = open_csv('ES.csv')[1:]
+    es = list(map(fix_24, es))
+    dc = scipy.array(es)
+    date_tuple = set([item[0].date() for item in dc])
+    min_date = min(date_tuple)
+    dc1 = all_for_day(min_date, dc)
+    my_plot(dc1[:, 0], dc1[:, 2])
+    dc2 = all_for_day(min_date+datetime.timedelta(days=20), dc)
+    my_plot(dc2[:, 0], dc2[:, 2])
